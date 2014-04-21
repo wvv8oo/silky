de06d518eec3858a3c326225d1b3e987bd169a79
Conis <Conis.yi@gmail.com>
2014-04-21 16:48:40 +0800

    示例配置文件

---
+++ samples/.silky/config.js
@@ -0,0 +1,15 @@
+/*
+    导出的配置文件
+ */
+module.exports = {
+    //默认使用80端口，*nix下需要sudo
+    port: 80,
+    //代理配置相关，兼容json-proxy的代理配置
+    proxy: {
+
+    },
+    //build的配置
+    build: {
+
+    }
+}
