931bdf0ba9172eb794e1020550a2c26961f878af
wvv8oo <wvv8oo@gmail.com>
2014-04-24 16:30:41 +0800

    配置文件增加输出，压缩，复制，编译设置等

--- samples/.silky/config.js
+++ samples/.silky/config.js
@@ -10,9 +10,40 @@ module.exports = {
     },
     //build的配置
     build: {
-
+        //构建的目标目录，命令行指定的优先
+        output: "./build",
+        //是否压缩
+        compress: {
+            //压缩js，包括coffee
+            js: true,
+            //压缩css，包括less
+            css: true,
+            //压缩html
+            html: false,
+            //是否压缩internal的js
+            internal: true
+        },
+        //将要复制的文件目录，直接复制到目标
+        copy: ["images"],
+        //将要编译处理的目录，如果存在less/coffee，则会直接编译
+        compile: {
+            //将template直接输出到目标目录下
+            "template":{
+                //保存到目标
+                target: './',
+                //要忽略的路径
+                ignore: /module$/i
+            },
+            //编译js目录
+            "js": {},
+            //编译css目录
+            "css": {
+                //忽略目标目录
+                ignore: /module$/i
+            }
+        }
     },
-    //将要监控哪些文件目录
+    //将要监控哪些文件目录，当监控中的内容被更改后，会触发客户端的自动更新事件
     watch: {
         //监控js目录，以js和coffee结尾的，将被监控
         "js": /(js|coffee)$/i,
