e9949fbc656001e248e045c068aa5e62a2c84550
wvv8oo <wvv8oo@gmail.com>
2015-09-02 16:13:22 +0800

    增加编译器相关的示例配置

--- samples/default/.silky/config.js
+++ samples/default/.silky/config.js
@@ -15,10 +15,33 @@ module.exports = {
             //"/ajax": "/"
         }
     },
+    /*
+    //编译器相关
+    compiler: {
+        //根据规则捕获文件路径，并指定路由
+        //注意，路径捕获规则的优先级要高于扩展名规则
+        rules: [
+            {
+                path: /^index\.(html|jade)$/i, compiler: 'jade'
+            }
+        ],
+        //根据扩展名指定编译器，这也是默认的编译器配置
+        extension: {
+            htm: 'hbs',
+            html: 'hbs',
+            css: 'less',
+            js: 'coffee'
+        }
+    },
+    */
     //如果不需要启用livereload，请注释掉livereload，默认监控扩展名为：'less', 'coffee', 'hbs', 'html', 'css'
     livereload: {},
     //路由
     routers: [
+        {
+          //将所有扩展名为js的重命名为coffee，指定编译器为coffee
+          path: /^(.+).js$/i, to: '$1.coffee', compiler: 'coffee', next: false
+        },
         //如果希望访问目录直接访问index.html，则可以启用下面的路由
         {
             //path: 原路径，
@@ -42,6 +65,15 @@ module.exports = {
         }
         */
     },
+    //合并规则
+    merge: [
+        {
+            //合并后的文件名
+            target: "main.js",
+            //捕获的规则
+            matches: []
+        }
+    ],
     //build的配置
     build: {
         //构建的目标目录，命令行指定的优先
